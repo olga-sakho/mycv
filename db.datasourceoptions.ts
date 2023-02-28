4f00e79d7db92e5877eb6e61d3dd43bffaf87e92
olga-sakho <sakho.olga93@gmail.com>
2023-02-28 10:14:51 +0200

    initial commit

---
+++ db.datasourceoptions.ts
@@ -0,0 +1,36 @@
+const _DBOptions = {
+    type: "sqlite",
+    entities: [__dirname + '/**/*.entity{.js,.ts}'],
+    synchronize: true,
+    migrations: [__dirname + "/migrations/*.js"]
+};
+  
+switch (process.env.NODE_ENV) {
+    case 'development':
+        Object.assign(_DBOptions, {
+            type: 'sqlite',
+            database: 'db.sqlite',
+        });
+        break;
+    case 'test':
+        Object.assign(_DBOptions, {
+            type: 'sqlite',
+            database: 'test.sqlite',
+            migrationsRun: true,
+        });
+        break;
+    case 'production':
+        Object.assign(_DBOptions, {
+            type: 'postgres',
+            url: process.env.DATABASE_URL,
+            migrationsRun: true,
+            ssl: {
+                rejectUnauthorized: false,
+            },
+        });
+        break;
+    default:
+        throw new Error('unknown environment');
+}
+
+export const DBOptions = _DBOptions;
---
+++ src/app.module.ts
@@ -0,0 +1,81 @@
+import { MiddlewareConsumer, Module, ValidationPipe } from '@nestjs/common';
+import { APP_PIPE } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
+import { config } from 'process';
+import { DBOptions } from '../db.datasourceoptions';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+const cookieSession = require('cookie-session')
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (config: ConfigService) => {
+        const dbOptions: TypeOrmModuleOptions = {
+          // retryAttempts: 10,
+          // retryDelay: 3000,
+          // autoLoadEntities: false
+        };
+
+        Object.assign(dbOptions, DBOptions);
+
+        return dbOptions;
+      },
+    }),
+    // TypeOrmModule.forRoot(dbConfig),
+    // TypeOrmModule.forRootAsync({
+    //   inject: [ConfigService],
+    //   useFactory: (config: ConfigService) => {
+    //     return {
+    //       type: 'sqlite',
+    //       database: config.get<string>('DB_NAME'),
+    //       synchronize: true,
+    //       entities: [User, Report]
+    //     }
+    //   }
+    // }),
+    // TypeOrmModule.forRoot({
+    //   type: 'sqlite',
+    //   database:'db.sqlite',
+    //   entities: [User, Report],
+    //   synchronize: true
+    // }), 
+    UsersModule, 
+    ReportsModule
+  ],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+      }),
+    },
+  ],
+})
+export class AppModule {
+  constructor(
+    private configService: ConfigService
+  ) {}
+
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        cookieSession({
+          keys: [this.configService.get('COOKIE_KEY')]
+        })
+      )
+      .forRoutes('*')
+  }
+}
